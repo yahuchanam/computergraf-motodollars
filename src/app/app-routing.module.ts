abea45f11e4c882e2d5cab2a53b3c1de3ca7defb
Marcus Borges <yahuchanam@gmail.com>
2022-12-01 12:01:38 -0300

    feat: init exchange route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'exchange'
+  },
+  {
+    path: 'exchange',
+    loadComponent: () =>
+      import('./presentation/exchange/exchange.component').then(
+        (m) => m.ExchangeComponent
+      )
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/presentation/exchange/exchange.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-exchange',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './exchange.component.html',
+  styleUrls: ['./exchange.component.scss']
+})
+export class ExchangeComponent {}
