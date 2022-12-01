7cb897b06da25ddf7082a097c3c63cbcb02d9971
Marcus Borges <yahuchanam@gmail.com>
2022-12-01 08:38:17 -0300

    chore: add external API settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  distanceCalcAPI: 'https://dev.virtualearth.net/REST/V1/Routes/Driving',
+  dollarRateAPI: 'https://economia.awesomeapi.com.br/json/daily/USD',
+  searchCEPAPI: 'https://api.postmon.com.br/v1/cep/'
 };
