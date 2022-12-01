155df86d53b5029adcdc89ab6285c3b35db76bdd
Marcus Borges <yahuchanam@gmail.com>
2022-12-01 08:27:37 -0300

    feat: add app model

---
+++ src/app/model/address.interface.ts
@@ -0,0 +1,11 @@
+export interface Address {
+  id: number;
+  address: string;
+  addressNumber: string;
+  extra: string;
+  district: string;
+  city: string;
+  state: string;
+  postalCode: string;
+  kmDistance: number;
+}
--- src/app/model/customer.interface.ts
+++ src/app/model/customer.interface.ts
@@ -1,3 +1,9 @@
+import { Address } from './address.interface';
+import { Order } from './order.interface';
+
 export interface Customer {
   id: number;
+  name: string;
+  address: Address;
+  orders: Order[];
 }
